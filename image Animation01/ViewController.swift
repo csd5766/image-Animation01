6272808c52da539bdb39d645ac3761a7e3138e79
D7702_09 <d7702_09@d7702-09ui-imac.local>
2019-03-27 14:58:06 +0900

    외계인 그림 교체  Count

--- image Animation01/ViewController.swift
+++ image Animation01/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var alienImageView: UIImageView!
+    var count = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        alienImageView.image = UIImage(named: "frame\(count).png")
+        
     }
 
 
+    @IBAction func btnUd(_ sender: Any) {
+        //print("button")
+        if(count<5){
+        count+=1
+        alienImageView.image = UIImage(named: "frame\(count).png")
+        //myLabel.text = String(count)
+           myLabel.text = "frame\(count).png"
+            
+        
+    }
+        else if(count == 5){
+           count=0
+        }
+    
+    
+    }
+    
 }
 
